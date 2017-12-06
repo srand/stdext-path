d5d239a8eef9a4ce09e5a37076bf6f5845622b71
Robert Andersson <rbrt.andersson@outlook.com>
2017-12-06 21:09:40 +0100

    Add pam build definition

---
+++ pam.py
@@ -0,0 +1,16 @@
+from build.model import *
+from externals.googletest import googletest
+from toolchains.pam import *
+
+
+stdext_path = cxx_library(
+    name = "stdext-path",
+    incpaths = [("include", {"publish": True})]
+)
+
+stdext_path_test = cxx_executable(
+    name = "stdext-path-test",
+    sources = ["test/test.cpp"],
+    dependencies = [stdext_path, googletest],
+    features = ["language-c++11"]
+)
